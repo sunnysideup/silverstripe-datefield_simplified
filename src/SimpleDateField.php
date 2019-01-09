c00a4feb8c234db779d7af6dd511aaf54a42fef7
Nicolaas <github@sunnysideup.co.nz>
2019-01-09 20:04:54 +1300

    PATCH: automated commit

--- src/SimpleDateField.php
+++ src/SimpleDateField.php
@@ -61,7 +61,7 @@ class SimpleDateField extends DateField
     {
         //GENERAL
         Requirements::javascript('silverstripe/admin: thirdparty/jquery/jquery.js');
-        Requirements::javascript("sunnysideup/datefield_simplified: datefield_simplified/javascript/SimpleDateField.js");
+        Requirements::javascript("sunnysideup/datefield_simplified: client/javascript/SimpleDateField.js");
         $this->addExtraClass("simpledatefield");
         $this->setAttribute("placeholder", $this->Config()->get("placeholder_value"));
         $html = parent::Field($options);
